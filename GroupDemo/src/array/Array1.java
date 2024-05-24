c880b5f6fcc4a66efc1765e1a936c264b6dda246
fullstackltd <fullstackltd@gmail.com>
2024-05-24 22:04:48 +0600

    Basic Push

--- GroupDemo/src/array/Array1.java
+++ GroupDemo/src/array/Array1.java
@@ -4,7 +4,7 @@ public class Array1 {
 public static void main(String[] args) {
 		
 		int [] num = {10,20,30,40,50,60,70,80,90,100};
-		
+		// from github.com/TahsinAyman
 		int sum = 0;
 		for(int x : num) {
 			System.out.println(x);
