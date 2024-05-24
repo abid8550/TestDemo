28b8711604a56794abaa613535b40d92edc73be3
abid8550 <abid8550@gmail.com>
2024-05-24 18:39:58 +0600

    First Commit

---
+++ GroupDemo/src/array/Array1.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Array1 {
+public static void main(String[] args) {
+		
+		int [] num = {10,20,30,40,50,60,70,80,90,100};
+		
+		int sum = 0;
+		for(int x : num) {
+			System.out.println(x);
+			sum = sum+x;
+			
+		}
+		System.out.println("Sum: "+sum);
+	}
+}
